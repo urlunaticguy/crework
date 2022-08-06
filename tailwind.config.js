e296850b55bc6125a86058e6d4f32c74eb42270b
urlunaticguy <85105608+urlunaticguy@users.noreply.github.com>
2022-08-06 15:12:30 +0530

    added universal tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+            "./Week 3 - Cloning Website using Tailwind CSS/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
